1239172d11faaa547f5fca7339a7672b9eece296
hy <balltangtang@naver.com>
2018-08-30 16:30:44 +0900

    MyBatis와 객체를 이용해서 User, Student 객체 조회하기 (1:1 관계)

--- spring4.3project/src/myspring/user/test/UserTest.java
+++ spring4.3project/src/myspring/user/test/UserTest.java
@@ -1,16 +1,21 @@
 package myspring.user.test;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import myspring.user.vo.StudentVO;
+import myspring.user.vo.UserVO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:config/main.xml")
@@ -25,6 +30,24 @@ public class UserTest {
 	SqlSession sqlSession;
 	
 	@Test
+	public void studentTable() {
+		List<StudentVO> stuList = sqlSession.selectList("studentNS.selectStudentDeptById");
+		
+		for (StudentVO studentVO : stuList) {
+			System.out.println(studentVO);
+		}
+	}
+	
+	@Test @Ignore
+	public void usersTable() {
+		List<UserVO> userList = sqlSession.selectList("userNS.selectUserList");
+		
+		for (UserVO userVO : userList) {
+			System.out.println(userVO);
+		}
+	}
+	
+	@Test @Ignore
 	public void connection() {
 		
 		System.out.println(sqlSessionFactory.getClass().getName());
